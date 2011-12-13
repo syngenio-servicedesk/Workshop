7b2bc8a7febc1ff78023dc2f73f13913d508355a
Peter Bilstein <peter.bilstein@syngenio.de>
2011-12-13 15:35:23 +0100

    implemented create service

--- src/de/syngenio/lib/service/BookCreateService.java
+++ src/de/syngenio/lib/service/BookCreateService.java
@@ -1,10 +1,21 @@
 package de.syngenio.lib.service;
 
+import de.syngenio.lib.domainobject.Book;
+import de.syngenio.lib.io.CharacterReader;
+
 public class BookCreateService extends BookServiceBase{
 
+	
+	
 	@Override
 	public void optionSelected() {
-		System.out.println("Operation create not supported yet.");
+		System.out.println("Neues Buch anlegen.");
+		String name = CharacterReader.readString("Name:");
+		String isbn = CharacterReader.readString("ISBN:");
+		Book bookToSave = new Book();
+		bookToSave.setName(name);
+		bookToSave.setIsbn(isbn);
+		bookDao.saveBook(bookToSave );
 	}
 
 }
