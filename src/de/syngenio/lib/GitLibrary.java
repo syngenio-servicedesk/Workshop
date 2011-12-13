19684ab527ae2bb5186ff2a6bca047323eac9e52
Rene Bredlau <rene.bredlau@syngenio.de>
2011-12-13 15:50:13 +0100

    implemented  ui for book deletion

--- src/de/syngenio/lib/GitLibrary.java
+++ src/de/syngenio/lib/GitLibrary.java
@@ -29,12 +29,13 @@ public class GitLibrary {
 	private BookCreateService bookCreationService  = new BookCreateService();
 	private BookEditService   bookEditService      = new BookEditService();
 
-	private IMenuChoiceService bookDeletionService = new BookDeletionService();
+	private BookDeletionService bookDeletionService = new BookDeletionService();
 	private IMenuChoiceService bookRentService     = new BookRentService();
 	private IMenuChoiceService bookReturnService   = new BookReturnService();
 
 	public GitLibrary() {
 		bookShowService.setBookDao(new BookDao());
+		bookDeletionService.setBookDao(new BookDao());
 	}
 
 	/**
--- src/de/syngenio/lib/service/BookDeletionService.java
+++ src/de/syngenio/lib/service/BookDeletionService.java
@@ -1,10 +1,26 @@
 package de.syngenio.lib.service;
 
+import de.syngenio.lib.dao.BookDao;
+import de.syngenio.lib.io.CharacterReader;
+
 public class BookDeletionService implements IMenuChoiceService{
+
+	private BookDao bookDao;
 
 	@Override
 	public void optionSelected() {
-		System.out.println("Option not supported, please come back later");
+		System.out.println("Welches Buch soll gelöscht werden:");
+		Integer bookId = CharacterReader.readIntegerFromConsole();
+		bookDao.deleteBook(bookId);
 	}
+
+	public BookDao getBookDao() {
+		return bookDao;
+	}
+
+	public void setBookDao(BookDao bookDao) {
+		this.bookDao = bookDao;
+	}
+
 
 }
